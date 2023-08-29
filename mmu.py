4a89e95e5e72eae3fff7ae5114136f24835f7ce1
Andrew <57894240+Arutosu@users.noreply.github.com>
2023-08-29 21:33:23 +0930

    Update mmu.py - Implemented additional methods
    Implemented methods which can be used by the child classes

--- mmu.py
+++ mmu.py
@@ -8,23 +8,66 @@
 *
 '''
 class MMU:
+
+    page_table = []             ## Page_table is a list of pages of the form -
+                                ## [[page_number, dirty_bit], ..., [page_number, dirty_bit]]
+
+    def __init__(self, frames):
+        self.frames = frames
+        self.total_disk_reads = 0
+        self.total_disk_writes = 0
+        self.total_page_faults = 0
+        self.debug_mode = ""
+
+        for i in range(frames):
+            self.page_table.append([None, None])
+
+
     def read_memory(self, page_number):
-        pass
+        """ 
+        This method is called when a page is read from memory.
+        The method calls find_page_number to find the page number in the page table.
+        Params: page_number - The page number to be read
+        Returns: Same return value as find_page_number()
+        """
+        return self.find_page_number(page_number)
 
     def write_memory(self, page_number):
         pass
 
+    def find_page_number(self, page_number):
+        """ 
+        Finds the page number in the page table and returns the index of the page.
+        Params: page_number - The page number to be found
+        Returns: index of page_number in page_table, -1 if page_number not found 
+        """
+        for i in range(len(self.page_table)):
+            if self.page_table[i][0] == page_number:
+                return i
+        return -1
+    
+    def find_empty_frame(self):
+        """
+        
+        """
+        pages = [page[0] for page in self.page_table]  # List of page numbers in page table
+        if(pages.count(None) > 0):
+            return pages.index(None)                   # Return index of first empty frame
+
+        return False                                   # Return False if no empty frames
+        
+
     def set_debug(self):
-        pass
+        self.debug_mode = "debug"
 
     def reset_debug(self):
-        pass
+        self.debug_mode = "quiet"
 
     def get_total_disk_reads(self):
-        return -1
+        return self.total_disk_reads
 
     def get_total_disk_writes(self):
-        return -1
+        return self.total_disk_writes
 
     def get_total_page_faults(self):
-        return -1
+        return self.total_page_faults
