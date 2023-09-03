81fb40555bf2bb4a1dddaeaa8c0883d0b0dda350
arham1117 <arhambinahmer23@gmail.com>
2023-09-04 00:09:36 +0930

    clock implemented

--- clockmmu.py
+++ clockmmu.py
@@ -3,8 +3,12 @@ from mmu import MMU
 
 class ClockMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for EscMMU
-        pass
+        super().__init__(frames)
+        self.hand = 0 
+        self.total_disk_reads = 0
+        self.total_disk_writes = 0
+        self.total_page_faults = 0
+  
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
@@ -15,21 +19,83 @@ class ClockMMU(MMU):
         pass
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        empty_frame = self.find_empty_frame()
+        page_index = self.find_page_number(page_number)
+        
+        if page_index is not False:
+        # Page is already in memory, update the use bit to 1
+            self.page_table[page_index][2] = 1
+            
+        elif empty_frame is not False:
+            self.total_disk_reads+=1
+            self.total_page_faults+=1
+            self.page_table[empty_frame]=[page_number,0,1]
+        else:
+            #self.total_disk_reads+=1
+            #self.total_page_faults+=1
+            self.replaceCLOCK(page_number,"r")
 
+        
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        empty_frame = self.find_empty_frame()
+        page_index = self.find_page_number(page_number)
+        if page_index is not False:
+            self.page_table[page_index][1] = 1
+            self.page_table[page_index][2] = 1
+            return
+        elif empty_frame is not False:
+            self.total_disk_reads+=1
+            self.total_page_faults+=1
+            self.page_table[empty_frame]=[page_number,1,1]
+        else:
+            #self.total_disk_reads+=1
+            #self.total_page_faults+=1
+            self.replaceCLOCK(page_number,"w")
 
     def get_total_disk_reads(self):
         # TODO: Implement the method to get total disk reads
-        return -1
+        return self.total_disk_reads
 
     def get_total_disk_writes(self):
         # TODO: Implement the method to get total disk writes
-        return -1
+        return self.total_disk_writes
 
     def get_total_page_faults(self):
         # TODO: Implement the method to get total page faults
-        return -1
+        return  self.total_page_faults
+    
+    def replaceCLOCK(self, page_number,operation):
+        self.total_disk_reads+=1
+        self.total_page_faults+=1
+        while True:
+            #current_page = self.page_table[self.hand]
+            
+            if self.page_table[self.hand][2]== 0:  
+                if self.page_table[self.hand][1] == 0:
+                    
+                    if (operation=="r"):
+                       self.page_table[self.hand] = [page_number, 0, 1] 
+                    elif (operation == "w"):
+                       self.page_table[self.hand] = [page_number, 1, 0] 
+
+                    self.hand = (self.hand + 1) % self.frames 
+        
+                    return
+                    
+                else:
+                    print(page_number)
+                    self.total_disk_writes += 1
+
+                    if (operation=="r"):
+                        self.page_table[self.hand] = [page_number, 0, 1]  
+                    elif (operation == "w"):
+                        self.page_table[self.hand] = [page_number, 1, 0]  
+
+                    self.hand = (self.hand + 1) % self.frames
+                                    
+                    return
+                     
+            else:
+                self.page_table[self.hand][2]= 0
+                self.hand = (self.hand + 1) % self.frames
+        
